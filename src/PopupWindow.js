4e43713889f833aae11ad04c2879b7c401a8d436
Miguel Ángel Quintanilla <mangel@profile.es>
2019-10-03 12:57:09 +0200

    Add Spotify Authorization Code Flow login support

--- src/PopupWindow.js
+++ src/PopupWindow.js
@@ -1,79 +1,95 @@
 import { toParams, toQuery } from './utils';
 
 class PopupWindow {
-  constructor(id, url, options = {}) {
-    this.id = id;
-    this.url = url;
-    this.options = options;
-  }
-
-  open() {
-    const { url, id, options } = this;
-
-    this.window = window.open(url, id, toQuery(options, ','));
-  }
-
-  close() {
-    this.cancel();
-    this.window.close();
-  }
-
-  poll() {
-    this.promise = new Promise((resolve, reject) => {
-      this._iid = window.setInterval(() => {
-        try {
-          const popup = this.window;
-
-          if (!popup || popup.closed !== false) {
-            this.close();
-
-            reject(new Error('The popup was closed'));
-
-            return;
-          }
-
-          if (popup.location.href === this.url || popup.location.pathname === 'blank') {
-            return;
-          }
-
-          const params = toParams(popup.location.hash.replace(/^#/, ''));
-
-          resolve(params);
-
-          this.close();
-        } catch (error) {
-          /*
-           * Ignore DOMException: Blocked a frame with origin from accessing a
-           * cross-origin frame.
-           */
-        }
-      }, 500);
-    });
-  }
-
-  cancel() {
-    if (this._iid) {
-      window.clearInterval(this._iid);
-      this._iid = null;
+    constructor(responseType, id, url, options = {}) {
+        this.responseType = responseType;
+        this.id = id;
+        this.url = url;
+        this.options = options;
     }
-  }
 
-  then(...args) {
-    return this.promise.then(...args);
-  }
+    open() {
+        const { url, id, options } = this;
 
-  catch(...args) {
-    return this.promise.then(...args);
-  }
+        this.window = window.open(url, id, toQuery(options, ','));
+    }
 
-  static open(...args) {
-    const popup = new this(...args);
+    close() {
+        this.cancel();
+        this.window.close();
+    }
 
-    popup.open();
-    popup.poll();
+    poll() {
+        this.promise = new Promise((resolve, reject) => {
+            this._iid = window.setInterval(() => {
+                try {
+                    const popup = this.window;
 
-    return popup;
-  }
+                    if (!popup || popup.closed !== false) {
+                        this.close();
+
+                        reject(new Error('The popup was closed'));
+
+                        return;
+                    }
+
+                    if (popup.location.href === this.url || popup.location.pathname === 'blank') {
+                        return;
+                    }
+
+
+                    let params
+
+                    if (this.responseType === "token") {
+                        params = toParams(popup.location.hash.replace(/^#/, ''));
+                    } else if (this.responseType === "code") {
+                        const urlParams = new URLSearchParams(popup.location.search);
+                        const code = urlParams.get('code') ? urlParams.get('code') : "";
+                        params = { code }
+                    } else {
+                        this.close();
+
+                        reject(new Error('The popup was closed'));
+
+                        return; 
+                    }
+
+                    resolve(params);
+
+                    this.close();
+                } catch (error) {
+                    /*
+                     * Ignore DOMException: Blocked a frame with origin from accessing a
+                     * cross-origin frame.
+                     */
+                }
+            }, 500);
+        });
+    }
+
+    cancel() {
+        if (this._iid) {
+            window.clearInterval(this._iid);
+            this._iid = null;
+        }
+    }
+
+    then(...args) {
+        return this.promise.then(...args);
+    }
+
+    catch(...args) {
+        return this.promise.then(...args);
+    }
+
+    static open(...args) {
+        const popup = new this(...args);
+
+        popup.open();
+        popup.poll();
+
+        return popup;
+    }
 }
 
 export default PopupWindow;
--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,27 @@
-export function toParams(query) {
+export const toParams = (query) => {
   const q = query.replace(/^\??\//, '');
 
+  console.log("query ", query)
+
   return q.split('&').reduce((values, param) => {
-    const [key, value] = param.split('=');
+      const [key, value] = param.split('=');
 
-    values[key] = value;
+      values[key] = value;
 
-    return values;
+      return values;
   }, {});
 }
 
-export function toQuery(params, delimiter = '&') {
+export const toQuery = (params, delimiter = '&') => {
   const keys = Object.keys(params);
 
   return keys.reduce((str, key, index) => {
-    let query = `${str}${key}=${params[key]}`;
+      let query = `${str}${key}=${params[key]}`;
 
-    if (index < (keys.length - 1)) {
-      query += delimiter;
-    }
+      if (index < (keys.length - 1)) {
+          query += delimiter;
+      }
 
-    return query;
+      return query;
   }, '');
 }
